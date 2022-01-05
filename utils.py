1e9c809b216ff52afbf2b90881dba07229c3fe06
Aayush Garg <aayushgargiitr@gmail.com>
2022-01-05 14:23:48 +0530

    Update utils and download more stock data

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@
 
 import yfinance as yf
 import pandas as pd
+import json
 
 # Load historical data in the past 10 years
 def read_single_stock(symbol='RELIANCE.NS', period='1y'):
@@ -37,9 +38,69 @@ def write_csv(dataframe, outname='./data/sample_stock.csv'):
     ------
     None
     '''
+    print('Write {}'.format(outname))
     dataframe.to_csv(outname)
+
+def write_json(in_dict, outname='./data/multiple_stocks.json'):
+    '''
+    Write a dict as json
     
+    Input parameters
+    ----------------
+    in_dict : dictionary to write
+    outname : file to save the data
+    
+    Output
+    ------
+    None
+    '''
+    print('Write {}'.format(outname))
+    with open(outname, 'w') as outfile:
+        json.dump(in_dict, outfile)
+
+    
+def read_multiple_stocks(symbol=['RELIANCE.NS'], period='1y', save=True, outdir='./data/'):
+    '''
+    Read multiple stocks history data for specified period as dataframe
+    and save it (optional)
+    
+    Input parameters
+    ----------------
+    symbol : str, symbol for stock
+    period : str, time period ('1d', '1m', '1y'... )
+    save   : bool, write the df as csv or not
+    outdir : directory to save the data
+
+    Output
+    ------
+    write individual stock as dataframes (csv)
+    return multiple stocks as dict
+    '''
+
+    stock_dict = {}
+    for s in symbol:
+        ticker = yf.Ticker(s)
+        stock = ticker.history(period=period)
+        stock_dict[s] = stock
+
+        if save:
+            write_csv(stock, outname=outdir + s)
+
+    return stock_dict
+
 if __name__ == '__main__':
-    symbol = 'ADANIPORTS.NS'
-    df = read_single_stock(symbol)
-    write_csv(df)
+    # read and write single stock
+    symbol = 'ADANIPORTS'
+    df = read_single_stock(symbol + '.NS')
+    write_csv(df, outname='./data/sample_stock.csv')
+
+    # read and write multiple stocks
+    symbol = ['ADANIPORTS', 'ALKEM', 'ASHOKA',
+            'ASHOKLEY', 'ASIANPAINT', 'BAJFINANCE',
+            'BRITANNIA', 'CAMS', 'COALINDIA']
+    for i in range(len(symbol)):
+        symbol[i] +='.NS'
+
+    read_multiple_stocks(symbol, period='1y', save=True, outdir='./data/')
+
+
